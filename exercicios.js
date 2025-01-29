9cf1fd121c74830962788629d866ca2a652600aa
ren164 <sus84467@gmail.com>
2025-01-29 08:47:49 -0300

    Update exercicios.js

---
+++ exercicios.js
@@ -0,0 +1,16 @@
+//ex1
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let div = a / b
+let mul = a * b
+
+
+console.log(soma)
+console.log(sub)
+console.log(div)
+console.log(mul)
+
+asdf
---
+++ operadores/aritimeticos.js
@@ -0,0 +1,25 @@
+console.log("Hello, World!")
+
+let nome = "juliana"
+var nomes = "juliana"
+
+console.log(nome)
+
+let idade = 24
+console.log("minha idade é", idade, "anos")
+
+// Operadores matematicos
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let div = a / b
+let mul = a * b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(div)
+console.log(mul)
+console.log(resto)
