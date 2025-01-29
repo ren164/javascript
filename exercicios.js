4968b3afe2a6c0b322906ae9d4135e263aee8622
ren164 <sus84467@gmail.com>
2025-01-29 13:10:50 -0300

    atualização

--- exercicios.js
+++ exercicios.js
@@ -4,13 +4,46 @@ let b = 2
 
 let soma = a + b
 let sub = a - b
-let div = a / b
 let mul = a * b
+let div = a / b
+let resto = a % b
 
 
 console.log(soma)
 console.log(sub)
 console.log(div)
 console.log(mul)
+console.log(resto)
 
-asdf
+//ex2
+let c = 4
+let d = 8
+
+let mult = c * d
+console.log("area:", mult)
+
+//ex3
+
+let numA = 3
+
+let Mul = numA * 2
+let Div = numA / 2
+console.log(Mul)
+console.log(Div)
+
+//ex4
+
+let n1 = 6
+let n2 = 9
+let n3 = 10 
+
+let media =(n1 + n2 + n3)/3
+console.log("media:", media)
+
+//ex5
+
+let C = 17
+
+let F = (C * 9/5) + 32
+
+console.log ("fahrenheit:", F)
---
+++ exercicios2.js
@@ -0,0 +1,54 @@
+//ex1
+
+let m = 3
+let mn = 2
+
+let maior = m > mn
+console.log(maior)
+
+let igual = m <= mn
+console.log(igual)
+
+let menor = m < mn
+console.log(menor)
+
+//ex2
+
+let a = id>=18 && id<=70
+let id = 18
+let voto = id >16
+
+let nao =id > voto
+console.log(nao)
+
+let sim = a 
+console.log(sim)
+
+//ex3
+
+let jovem = jovem => 18
+console.log(jovem)
+
+let idoso = idoso => 60
+console.log(idoso)
+
+
+//ex4
+
+let user = "amir"
+console.log("amir")
+
+//ex5
+
+let N1 = 10
+
+let div1 = 5 % N1 == 0
+let div2 = 3 % N1 == 0
+
+//ex 6
+
+let saldo = 1000
+let saque = 800
+
+let sub = saldo - saque
+console.log("restam:",sub)
