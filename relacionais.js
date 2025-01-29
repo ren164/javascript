654b6965eaa67da1f1d00c1c5ed8adfe131c423c
ren164 <sus84467@gmail.com>
2025-01-29 10:03:27 -0300

    att

---
+++ relacionais.js
@@ -0,0 +1,25 @@
+let idade = 18
+let resultado = idade > 18
+let resul = idade >= 18
+let res = idade <= 18
+let r = idade < 18
+let re= idade == 18
+let rest = idade != 18
+
+console.log(resultado)
+console.log(resul)
+console.log(res)
+console.log(r)
+console.log(re)
+console.log(rest)
+
+console.log("ioioioioioioioioioioio")
+console.log("aaaaaaaa")
+let dinheiro = 100
+let compra = 40
+let total = dinheiro - compra
+let situacao = total > 0
+console.log(situacao)
+
+console.log("ioioioioioioioioioioio")
+console.log("aaaaaaaa")
